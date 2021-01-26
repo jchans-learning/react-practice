c09d53e7d8d0c492d867fe655abe5d7d10a11ec0
jchans <jiangchenghans@gmail.com>
2021-01-26 15:24:05 +0800

    Practice creating a Class Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import CountFunc from './components/CountFunc'
+import CountClass from './components/CountClass'
 
 function App() {
-  return <CountFunc />
+  return (
+    <>
+      <h2>函式型元件</h2>
+      <CountFunc />
+      <h2>類別型元件</h2>
+      <CountClass />
+    </>
+  )
 }
 
 export default App
