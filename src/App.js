1a0fd09678b0ae3f3c0886ede09f2a3329e1294e
jchans <jiangchenghans@gmail.com>
2021-01-26 14:51:48 +0800

    practice01 counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-// 清空 App.js 來做初步練習
-
-import HelloWorld from './HelloWorld'
+import { useState } from 'react'
 
 function App() {
-  return <HelloWorld />
+  const [total, setTotal] = useState(0)
+
+  return <h1 onClick={() => setTotal(total + 1)}>{total}</h1>
 }
 
 export default App
