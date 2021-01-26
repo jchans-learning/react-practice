f95dceade0423ccdceaa02c741c8f08e9a10b715
jchans <jiangchenghans@gmail.com>
2021-01-26 14:56:12 +0800

    Counter with Buttons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import { useState } from 'react'
 function App() {
   const [total, setTotal] = useState(0)
 
-  return <h1 onClick={() => setTotal(total + 1)}>{total}</h1>
+  return (
+    <>
+      <h1>{total}</h1>
+      <button
+        onClick={() => {
+          setTotal(total - 1)
+        }}
+      >
+        -1
+      </button>
+      <button
+        onClick={() => {
+          setTotal(total + 1)
+        }}
+      >
+        +1
+      </button>
+    </>
+  )
 }
 
 export default App
