c01e4369f019a4492aed2b6464e1fbdf87b0c0b0
jchans <jiangchenghans@gmail.com>
2021-01-26 15:12:35 +0800

    create a function component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import { useState } from 'react'
+import CountFunc from './components/CountFunc'
 
 function App() {
-  const [total, setTotal] = useState(0)
-
-  return (
-    <>
-      <h1>{total}</h1>
-      <button
-        onClick={() => {
-          setTotal(total - 1)
-        }}
-      >
-        -1
-      </button>
-      <button
-        onClick={() => {
-          setTotal(total + 1)
-        }}
-      >
-        +1
-      </button>
-    </>
-  )
+  return <CountFunc />
 }
 
 export default App
